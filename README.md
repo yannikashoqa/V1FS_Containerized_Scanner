d098fffe2b93ce7b8488062ea3a06cc059f642cd
Yanni Kashoqa <yanal.kashoqa@outlook.com>
2025-10-13 14:47:14 -0400

    Add Mutil-Node cluster deployment

--- README.md
+++ README.md
@@ -12,7 +12,18 @@ REQUIRMENTS
 - Place all scripts and yaml files are in the same folder on the target server
 
 INSTRUCTIONS
-RHEL 9:
+RHEL 9 (Multi-Node Kubernetes Cluster):
+- Run the following script to install Kubernetes on the Master Node/Controller: Setup_Kubernetes\Install_Kubernetes_RHEL9_Master.sh
+- Run the following script to install Kubernetes on the worker Nodes: Setup_Kubernetes\Install_Kubernetes_RHEL9_Node.sh
+- Once completed run the following commands on the Master Node to make sure all pods are running: kubectl get pods -A
+- Update Setup_LoadBallancer\PureLB_Config.yaml with the IP addresses and subnet mask to match your environment
+- Run the PureLB installer on the Master Node: Setup_LoadBallancer\Install_PureLB.sh
+- Once completed run the following commands to make sure all pods are running: kubectl get pods -A
+- Update the values.yaml file with your autoscaling needs
+- Run the V1FS installer on the Master Node: Install_V1FS_ICAP.sh
+- Once completed run the following commands to make sure all pods are running: kubectl get pods -A
+
+RHEL 9 (Single node All in One Cluster):
 - Run the following script to install Kubernetes: Setup_Kubernetes\Install_Kubernetes_RHEL9.sh
 - Once completed run the following commands to make sure all pods are running: kubectl get pods -A
 - Update Setup_LoadBallancer\PureLB_Config.yaml with the IP addresses and subnet mask to match your environment
@@ -22,7 +33,7 @@ RHEL 9:
 - Run the V1FS installer: Install_V1FS_ICAP.sh
 - Once completed run the following commands to make sure all pods are running: kubectl get pods -A
 
-Ubuntu 24:
+Ubuntu 24 (Single node All in One Cluster):
 - Run the following script to install Kubernetes: Setup_Kubernetes\Install_Kubernetes_Ubuntu24.sh
 - Once completed run the following commands to make sure all pods are running: kubectl get pods -A
 - Update Setup_LoadBallancer\MetalLB_Config.yaml with the IP addresses to match your environment
---
+++ Setup_Kubernetes/Install_Kubernetes_RHEL9_Node.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+host_Name=$(hostname)
+
+#Disable swap and SELinux
+sudo swapoff -a
+sudo sed -i '/ swap / s/^\(.*\)$/#\1/g' /etc/fstab
+# Comment the swap entry in /etc/fstab if the previous command did not work
+
+sudo setenforce 0
+sudo sed -i 's/^SELINUX=enforcing$/SELINUX=permissive/' /etc/selinux/config
+
+# Disable the firewall.  If enabled, the File Security backend-communicator pod will keep on crashing
+# Stop the firewall service
+sudo systemctl stop firewalld
+
+# Disable the firewall service to prevent it from starting at boot
+sudo systemctl disable firewalld
+
+# Add Firewall Rules on Master and Worker Nodes
+# On Master Node
+#sudo firewall-cmd --permanent --add-port={6443,2379,2380,10250,10251,10252,10257,10259,179}/tcp
+#sudo firewall-cmd --permanent --add-port=4789/udp
+#sudo firewall-cmd --permanent --add-port=443/tcp  # Added to avoid error:  dial tcp 10.96.0.1:443: connect: no route to host
+#sudo firewall-cmd --reload
+
+# On worker Nodes
+# sudo firewall-cmd --permanent --add-port={179,10250,30000-32767}/tcp
+# sudo firewall-cmd --permanent --add-port=4789/udp
+# sudo firewall-cmd --reload
+
+# Add Kernel Modules and Parameters
+sudo modprobe overlay
+sudo modprobe br_netfilter
+
+sudo tee /etc/modules-load.d/k8s.conf <<EOF
+overlay
+br_netfilter
+EOF
+
+sudo tee /etc/sysctl.d/kubernetes.conf <<EOT
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
+EOT
+
+sudo sysctl --system
+
+# install Containerd
+sudo dnf config-manager --add-repo https://download.docker.com/linux/rhel/docker-ce.repo
+sudo dnf install containerd.io -y
+sudo systemctl start containerd
+sudo systemctl enable containerd
+containerd config default | sudo tee /etc/containerd/config.toml >/dev/null 2>&1
+sudo sed -i 's/SystemdCgroup \= false/SystemdCgroup \= true/g' /etc/containerd/config.toml
+sudo systemctl restart containerd
+
+# Add Kubernetes Yum Repository
+cat <<EOF | sudo tee /etc/yum.repos.d/kubernetes.repo
+[kubernetes]
+name=Kubernetes
+baseurl=https://pkgs.k8s.io/core:/stable:/v1.30/rpm/
+enabled=1
+gpgcheck=1
+gpgkey=https://pkgs.k8s.io/core:/stable:/v1.30/rpm/repodata/repomd.xml.key
+exclude=kubelet kubeadm kubectl cri-tools kubernetes-cni
+EOF
+
+# Install Kubeadm, kubelet & kubectl
+sudo dnf install -y kubelet kubeadm kubectl --disableexcludes=kubernetes
+
+# Make kubelet persistant after reboot
+sudo systemctl enable kubelet
